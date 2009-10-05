f0604be3d3ba13b467ab9f2a64b0054935ce9797
AndrewZdorovtsov <AndrewZdorovtsov@softensity.com>
2009-10-05 20:29:05 +0300

    deleted objects

--- app/controllers/auth_controller.rb
+++
@@ -1,7 +0,0 @@
-# Superclass for all controllers that need authenticated user.
-class AuthController < ApplicationController
-
-  before_filter :login_required
-  before_filter :admin_required
-
-end
